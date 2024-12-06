930056bdbaa0bae71e2d082e13bb005bfbd7d3f2
Debesh-Acharya <debeshacharya30@gmail.com>
2024-12-06 12:39:07 +0530

    message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import {connectDB} from "./lib/db.js"
 
 dotenv.config()
@@ -11,6 +12,7 @@ const PORT = process.env.PORT
 app.use(express.json())//allows to fetch data
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is running on PORT:"+PORT)
